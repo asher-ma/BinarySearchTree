48a8c0acacaeaaaa5840e379c0e4cd530231862c
asher-ma <asherhmangel@gmail.com>
2026-05-14 02:43:44 +0000

    Implements AvlNode methods

---
+++ src/AvlNode.cpp
@@ -0,0 +1,21 @@
+#include "BinaryNode.h"
+#include "AvlNode.h"
+
+// Constructors
+template <typename ItemType>
+AvlNode<ItemType>::AvlNode() {}
+
+template <typename ItemType>
+AvlNode<ItemType>::AvlNode(const ItemType& anItem) : BinaryNode(anItem),
+    height(this.getHeight()) {}
+
+// Public methods
+template <typename ItemType>
+void AvlNode<ItemType>::setHeight(int height) {
+    this.height = height;
+}
+
+template <typename ItemType>
+int AvlNode<ItemType>::getHeight() const {
+    return height;
+}
