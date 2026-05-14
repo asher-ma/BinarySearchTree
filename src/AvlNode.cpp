ea93c5582c80045a2a9b49103403e28a0165d826
asher-ma <asherhmangel@gmail.com>
2026-05-14 05:03:03 +0000

    Bug fixes

--- src/AvlNode.cpp
+++ src/AvlNode.cpp
@@ -3,16 +3,15 @@
 
 // Constructors
 template <typename ItemType>
-AvlNode<ItemType>::AvlNode() : BinaryNode() {}
+AvlNode<ItemType>::AvlNode() : BinaryNode<ItemType>() {}
 
 template <typename ItemType>
-AvlNode<ItemType>::AvlNode(const ItemType& anItem) : BinaryNode(anItem),
-    height(this.getHeight()) {}
+AvlNode<ItemType>::AvlNode(const ItemType& anItem) : BinaryNode<ItemType>(anItem), height(1) {}
 
 // Public methods
 template <typename ItemType>
 void AvlNode<ItemType>::setHeight(int height) {
-    this.height = height;
+    this->height = height;
 }
 
 template <typename ItemType>
