2669f64098075ac9f7a6c8bfaa83c92c4029e50c
asher-ma <mangelas@uw.edu>
2026-05-14 00:02:28 +0000

    Adds necessary methods

---
+++ Include/AvlTree.h
@@ -0,0 +1,29 @@
+#ifndef _AVL_TREE_H_
+#define _AVL_TREE_H_
+
+#include "AvlNode.h"
+#include "BinarySearchTree.h"
+
+template <typename ItemType>
+class AvlTree : public BinarySearchTree<ItemType> {
+    private:
+        int getHeight(AvlNode<ItemType>* nodePtr) const ;
+        void updateHeight(AvlNode<ItemType>* nodePtr);
+        int getBalanceFactor(AvlNode<ItemType>* nodePtr) const ;
+        
+        AvlNode<ItemType>* rebalance(AvlNode<ItemType>* nodePtr);
+        AvlNode<ItemType>* rotateLeft(AvlNode<ItemType>* nodePtr);
+        AvlNode<ItemType>* rotateRight(AvlNode<ItemType>* nodePtr);
+        AvlNode<ItemType>* rotateLeftRight(AvlNode<ItemType>* nodePtr);
+        AvlNode<ItemType>* rotateRightLeft(AvlNode<ItemType>* nodePtr);
+
+    public:
+        AvlTree() : BinarySearchTree<ItemType>() {}
+        AvlTree(const ItemType& rootItem) : BinarySearchTree<ItemType>(rootItem) {}
+        AvlTree(const AvlTree<ItemType>& tree) : BinarySearchTree<ItemType>(tree) {}
+
+        bool add(const ItemType& newEntry); // Need to override to rebalance
+        bool remove(const ItemType& anEntry); 
+};
+
+#endif
